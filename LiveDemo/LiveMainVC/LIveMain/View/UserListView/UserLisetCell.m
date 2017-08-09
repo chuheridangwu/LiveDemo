a0c60cae29cc4d1c32040eb1223287d49b0a2ae5
chuheridangwu <153100950@qq.com>
2017-08-09 11:19:44 +0800

    添加SD

--- LiveDemo/LiveMainVC/LIveMain/View/UserListView/UserLisetCell.m
+++ LiveDemo/LiveMainVC/LIveMain/View/UserListView/UserLisetCell.m
@@ -10,9 +10,11 @@
 
 @interface UserLisetCell ()
 {
-    UIImageView *_imgView;
     UIImageView *_imgView1;
 }
+
+@property (nonatomic,strong)    UIImageView *imgView;
+
 @end
 
 @implementation UserLisetCell
@@ -26,10 +28,10 @@
         
         UIImageView *imgView = [[UIImageView alloc]initWithFrame:self.bounds];
 //        imgView.clipsToBounds = YES;
-        imgView.layer.borderWidth = 1.5;
-        imgView.layer.borderColor = [[UIColor whiteColor]CGColor];
-        imgView.layer.cornerRadius = imgView.frame.size.width / 2;
-        imgView.userInteractionEnabled = YES;
+//        imgView.layer.borderWidth = 1.5;
+//        imgView.layer.borderColor = [[UIColor whiteColor]CGColor];
+//        imgView.layer.cornerRadius = imgView.frame.size.width / 2;
+//        imgView.userInteractionEnabled = YES;
         [self.contentView addSubview:imgView];
         _imgView = imgView;
       
@@ -40,10 +42,11 @@
 }
 
 - (void)setUserImgURL:(NSString *)url{
-//    _imgView.image = [[UIImage imageNamed:@"placeholder_head"] cutCircleImage];
+    _imgView.image = [[UIImage imageNamed:@"placeholder_head"] cutCircleImage];
+//    WS(ws);
 //    [_imgView1 sd_setImageWithURL:[NSURL URLWithString:url] placeholderImage:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
 //        if (image) {
-//            _imgView.image = [image cutCircleImage];
+//            ws.imgView.image = [image cutCircleImage];
 //        }
 //    }];
 }
--- Podfile
+++ Podfile
@@ -2,6 +2,8 @@ platform :ios, "8.0"
 
 target 'LiveDemo' do
 
+pod 'SDWebImage', '~> 3.7.5'
+
 #pod 'GPUImage', '~> 0.1.7' 由于LFLiveKit里面已经集成了GPUImage
 pod 'LFLiveKit', '~> 1.6'
 
