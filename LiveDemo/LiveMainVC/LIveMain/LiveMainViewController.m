c2e0309fca8bbb69519dff438a52d6eede8a0fa5
dym <dym@dymdemacbook-pro.local>
2017-08-21 15:21:25 +0800

    测试

--- LiveDemo/LiveMainVC/LIveMain/LiveMainViewController.m
+++ LiveDemo/LiveMainVC/LIveMain/LiveMainViewController.m
@@ -49,7 +49,8 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     [self.view addSubview:self.scrollView];
-//    [self.view addSubview:[self closePlayerBtn]];
+    [self.view addSubview:[self closePlayerBtn]];
+    
     
     [self resetScrollContent:_curIndex];
     
